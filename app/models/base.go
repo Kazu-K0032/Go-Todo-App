cac2868cf8a4bd2097f3929a85abe055a75cedbf
Kazu-K0032 <kuji32global@gmail.com>
2025-03-23 17:17:03 +0900

    feat: User追加の設定

--- app/models/base.go
+++ app/models/base.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
 	"log"
 
 	"todo_app/config"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -40,4 +42,14 @@ func init() {
 	if err != nil {
 		log.Fatalln("デーブルの作成に失敗しました", err)
 	}
+}
+
+func createUUID() (uuidobj uuid.UUID) {
+	uuidobj, _ = uuid.NewUUID()
+	return uuidobj
+}
+
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return cryptext
 }
