3e0eefd21068d2492c11119c44aa058983384461
Kazu-K0032 <kuji32global@gmail.com>
2025-03-23 18:07:24 +0900

    feat: todo関数の追加(todoの追加, 更新, 削除)

--- app/models/todos.go
+++ app/models/todos.go
@@ -25,3 +25,89 @@ func (u *User) CreateTodo(content string) (err error) {
 	}
 	return err
 }
+
+// Todo情報を取得する関数
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+		where id = ?`
+	todo = Todo{}
+	err = Db.QueryRow(cmd, id).Scan(
+		&todo.ID,
+		&todo.Content,
+		&todo.UserID,
+		&todo.CreatedAt,
+	)
+	return todo, err
+}
+
+// 全てのTodoを返す関数
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt,
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+}
+
+// 特定のユーザーのTodoを取得する関数
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+		where user_id = ?`
+
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt,
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+	return todos, err
+}
+
+// Todoを更新する関数
+func (t *Todo) UpdateTodo() error {
+	cmd := `update todos set content = ?, user_id = ?
+		where id = ?`
+	_, err = Db.Exec(cmd, t.Content, t.UserID, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+// Todoを削除する関数
+func (t *Todo) DeleteTodo() (err error) {
+	cmd := `delete from todos where id = ?`
+	_, err = Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
 	"todo_app/app/models"
 )
 
 func main() {
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
+	// u := &models.User{}
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.PassWord = "testtest"
+	// u.CreateUser()
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(3)
+	t.DeleteTodo()
 }
