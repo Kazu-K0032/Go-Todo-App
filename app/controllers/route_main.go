ad1e2d27f4c70d49f4f4f53cf7d70482d2e81e3c
Kazu-K0032 <kuji32global@gmail.com>
2025-03-23 20:58:04 +0900

    feat: 登録画面の裏側の処理の追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -6,5 +6,5 @@ import (
 
 // topのハンドラー
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "top")
+	generateHTML(w, "Hello", "layout", "public_navbar", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -20,6 +20,8 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 func StartMainSercer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
